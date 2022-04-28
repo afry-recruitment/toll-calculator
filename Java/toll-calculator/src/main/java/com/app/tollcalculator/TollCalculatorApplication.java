2f29c881a434e96d6c3c2a5cc9dbfd693b27a9e7
ravi sharma <ravi.s@inba-ravi.s-211109>
2022-04-29 00:25:00 +0530

    adding spring boot maven project

---
+++ Java/toll-calculator/src/main/java/com/app/tollcalculator/TollCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.app.tollcalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TollCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TollCalculatorApplication.class, args);
+	}
+
+}
