51bc9c73d9052290bf038f03e02b25b249d1ffad
Tehpson <jesper@familjen-pettersson.se>
2022-05-06 13:39:27 +0200

    added ApiHelper

---
+++ C#/ApiHelper.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace afryCodeTest.toll_calculator.C_
+{
+    public static class ApiHelper
+    {
+        public static HttpClient APIClient { get; set; } = new HttpClient();
+
+        public static void InitializeClient()
+        {
+            APIClient = new HttpClient
+            {
+                BaseAddress = new Uri("https://svenskahelgdagar.info/v2")
+            };
+            APIClient.DefaultRequestHeaders.Accept.Clear();
+            APIClient.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        public static async Task<DataResponse> getToday()
+        {
+            using (var response = await APIClient.GetAsync("/today"))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    return await response.Content.ReadAsAsync<DataResponse>();
+                }
+                else
+                {
+                    throw new Exception(response.ReasonPhrase);
+                }
+            }
+        }
+    }
+    public class DataResponse {
+        public bool public_sunday { get; set; }
+    }
+}
