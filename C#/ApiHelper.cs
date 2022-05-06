c4a2ae0121680322543432bc42f79c0d9b10073c
Tehpson <jesper@familjen-pettersson.se>
2022-05-06 15:37:52 +0200

    moved API key to hidden file

--- C#/ApiHelper.cs
+++ C#/ApiHelper.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
 using System.Net;
 
 namespace TollFeeCalculator
@@ -8,10 +6,10 @@ namespace TollFeeCalculator
     public static class ApiHelper
     {
         private const string BaseURL = "https://holidays.abstractapi.com/v1/";
-        private const string APIKey = "0383d039f45345608a90e50aec32297a";
+        private const string APIKey = Secrets.API_KEY;
+
         public static bool GetPublicHoliday(DateTime date)
         {
-
             var tokenRequest = WebRequest.Create(BaseURL + $"?api_key={APIKey}&country=SE&year={date.Year}&month={date.Month}&day={date.Day}");
             tokenRequest.Method = "GET";
 
