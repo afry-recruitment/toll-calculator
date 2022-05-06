4df64fb818ec719fdda2230437959cd5610dc35d
unknown <jeremy.matthiessen@biswise.se>
2022-05-06 12:04:10 +0200

    Added comments to the file.

--- C#/Vehicle.cs
+++ C#/Vehicle.cs
@@ -8,15 +8,24 @@ namespace TollFeeCalculator
 {
     public class Vehicle
     {
-        //String GetVehicleType();
-
+        /// <summary>
+        /// The <see cref="VehicleType"/> is a <see langword="string"/> that is used to get or set a vehicle type.
+        /// </summary>
         public string VehicleType { get; set; }
 
+        /// <summary>
+        /// The <see cref="Vehicle"/> is used to create a new vehicle.
+        /// </summary>
+        /// <param name="VehicleType">The <see langword="string"/> Vehicle type is used to say what type of vehicle it is.</param>
         public Vehicle(string VehicleType)
         {
             this.VehicleType = VehicleType;
         }
 
+        /// <summary>
+        /// The <see cref="GetVehicleType"/> is used to get the <seealso cref="VehicleType"/>.
+        /// </summary>
+        /// <returns>The <see cref="VehicleType"/>.</returns>
         public string GetVehicleType()
         {
             return VehicleType;
