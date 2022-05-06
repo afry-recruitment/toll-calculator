07b80fa968c9be422be02a14757ba62e246188ca
unknown <jeremy.matthiessen@biswise.se>
2022-05-06 15:38:52 +0200

    Updated the IsTollFreeDate method so that it check if the date is a holiday or not and with the correct dates.

--- C#/TollCalculator.cs
+++ C#/TollCalculator.cs
@@ -1,5 +1,12 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using PublicHoliday;
+using System;
+using System.Collections.Generic;
 using System.Globalization;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
 using TollFeeCalculator;
 
 public class TollCalculator
@@ -73,26 +80,8 @@ public class TollCalculator
 
     private Boolean IsTollFreeDate(DateTime date)
     {
-        int year = date.Year;
-        int month = date.Month;
-        int day = date.Day;
-
-        if (date.DayOfWeek == DayOfWeek.Saturday || date.DayOfWeek == DayOfWeek.Sunday) return true;
-
-        if (year == 2013)
-        {
-            if (month == 1 && day == 1 ||
-                month == 3 && (day == 28 || day == 29) ||
-                month == 4 && (day == 1 || day == 30) ||
-                month == 5 && (day == 1 || day == 8 || day == 9) ||
-                month == 6 && (day == 5 || day == 6 || day == 21) ||
-                month == 7 ||
-                month == 11 && day == 1 ||
-                month == 12 && (day == 24 || day == 25 || day == 26 || day == 31))
-            {
-                return true;
-            }
-        }
+        var publicHolidays = new SwedenPublicHoliday().IsPublicHoliday(date);
+        if (date.DayOfWeek == DayOfWeek.Saturday || date.DayOfWeek == DayOfWeek.Sunday || publicHolidays) return true;
         return false;
     }
 
