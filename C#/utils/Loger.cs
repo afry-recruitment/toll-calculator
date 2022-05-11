73913e5c77e9e5eb6dc0cca750de506741cfa5b7
Tehpson <jesper@familjen-pettersson.se>
2022-05-11 13:21:11 +0200

    created Loger file

---
+++ C#/utils/Loger.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace TollFeeCalculator
+{
+    class Loger
+    {
+        public string LogPath { get; } = Path.Combine(Directory.GetCurrentDirectory(), "logs");
+        public Loger()
+        {
+            Directory.CreateDirectory(LogPath);
+        }
+        public void LogPassing(Vehicle vehicle, DateTime dateTime)
+        {
+            var file = Path.Combine(LogPath, dateTime.ToString("yyyy-MM-dd"));
+            if (!File.Exists(file))
+            {
+
+            }
+        }
+    }
+}
