106f1fe56736759f8f94c505631947b9dc62850f
Nikitaj1991 <107789626+Nikitaj1991@users.noreply.github.com>
2022-06-20 23:17:47 +0530

    Added Toll Fee Calculator Console Application
    Added Toll Fee Calculator Console Application

---
+++ C#/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TollFeeCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            var vehical = new VehicleType("Car");
+            TollCalculator tc = new TollCalculator();
+            var staticDatetimes = new DateTime[] 
+            {
+                  new DateTime (2022,6,14,6,31,0),
+                  new DateTime(2022,6,14,7,10,0),
+                  new DateTime(2022,6,14,14,29,0)
+            };
+
+            int totalFee = tc.GetTotalTollFee(vehical, staticDatetimes.ToArray());
+
+          
+            /*
+            DateTime dateCheck;
+            List<DateTime> userDatetimes = new List<DateTime>();
+            string[] userDatetime = { "2022-6-14 7:35:0", "2022-6-14 13:35:0", "2022-14-14 19:31:0" };
+            foreach (string dateString in userDatetime)
+            {
+                if (DateTime.TryParse(dateString, out dateCheck))
+                {
+                    userDatetimes.Add(dateCheck);
+                }
+                else
+                {
+                    Console.WriteLine("  Unable to parse '{0}'.", dateString);
+                }
+            }
+
+            int totalFee = tc.GetTotalTollFee(vehical, userDatetimes.ToArray());
+            */
+
+            Console.WriteLine("Total Toll Fee :" + totalFee);
+            Console.ReadLine();
+           
+        }
+    }
+}
---
+++ C#/VehicleType.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TollFeeCalculator
+{
+    class VehicleType : Vehicle
+    {
+        private readonly string _vehicle;
+
+        public VehicleType(string vehicle)
+        {
+            _vehicle = vehicle;
+        }
+
+        public bool IsTollFreeVehicles()
+        {
+            switch (_vehicle)
+            {
+                case "Car":
+                    return false;
+                    break;
+                case "Emergency":
+                    return true;
+                    break;
+                case "Military":
+                    return true;
+                    break;
+                case "Tractor":
+                    return false;
+                    break;
+                case "Diplomat":
+                    return true;
+                    break;
+                case "Foreign":
+                    return true;
+                    break;
+            }
+
+            return false;
+        }
+    }
+}
