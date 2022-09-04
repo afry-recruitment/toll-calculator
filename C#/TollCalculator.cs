8095da6a4c808299f850c3c4c7199e0501a9c8bb
HadisSh <H.shirkosh81@gmail.com>
2022-09-04 13:15:02 +0200

    fixed calc 9,14 hourse fee

--- C#/TollCalculator.cs
+++ C#/TollCalculator.cs
@@ -63,7 +63,8 @@ public class TollCalculator
         else if (hour == 6 && minute >= 30 && minute <= 59) return 13;
         else if (hour == 7 && minute >= 0 && minute <= 59) return 18;
         else if (hour == 8 && minute >= 0 && minute <= 29) return 13;
-        else if (hour >= 8 && hour <= 14 && minute >= 30 && minute <= 59) return 8;
+        else if (hour == 8 && minute >= 30 && minute <= 59) return 8;
+        else if (hour >= 9 && hour <= 14 && minute >= 0 && minute <= 59) return 8;
         else if (hour == 15 && minute >= 0 && minute <= 29) return 13;
         else if (hour == 15 && minute >= 0 || hour == 16 && minute <= 59) return 18;
         else if (hour == 17 && minute >= 0 && minute <= 59) return 13;
