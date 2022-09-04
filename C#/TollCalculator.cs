6a276b690c4ce41afb5efabaf98a3aa3f51c5f9b
HadisSh <H.shirkosh81@gmail.com>
2022-09-04 13:17:34 +0200

    refactored IsTollFreeVehicle

--- C#/TollCalculator.cs
+++ C#/TollCalculator.cs
@@ -44,12 +44,8 @@ public class TollCalculator
     {
         if (vehicle == null) return false;
         String vehicleType = vehicle.GetVehicleType();
-        return vehicleType.Equals(TollFreeVehicles.Motorbike.ToString()) ||
-               vehicleType.Equals(TollFreeVehicles.Tractor.ToString()) ||
-               vehicleType.Equals(TollFreeVehicles.Emergency.ToString()) ||
-               vehicleType.Equals(TollFreeVehicles.Diplomat.ToString()) ||
-               vehicleType.Equals(TollFreeVehicles.Foreign.ToString()) ||
-               vehicleType.Equals(TollFreeVehicles.Military.ToString());
+        // if vehicleType exists in a name or integral value return true
+        return Enum.IsDefined(typeof(TollFreeVehicles), vehicleType);
     }
 
     public int GetTollFee(DateTime date, Vehicle vehicle)
