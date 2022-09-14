34e29543792b4d429c637deeb09eb78f10e11bfb
torbjornmolin <37022228+torbjornmolin@users.noreply.github.com>
2022-09-14 10:10:35 +0200

    Cleaned up fee if statement

--- C#/TollCalculator/TollCalculator.cs
+++ C#/TollCalculator/TollCalculator.cs
@@ -55,7 +55,7 @@ namespace TollFeeCalculator
                 return 8;
             else if (hour == 6 && minute >= 30 && minute <= 59)
                 return 13;
-            else if (hour == 7 && minute >= 0 && minute <= 59)
+            else if (hour == 7)
                 return 18;
             else if (hour == 8 && minute >= 0 && minute <= 29)
                 return 13;
@@ -65,9 +65,11 @@ namespace TollFeeCalculator
                 return 8;
             else if (hour == 15 && minute >= 0 && minute <= 29)
                 return 13;
-            else if (hour == 15 && minute >= 0 || hour == 16 && minute <= 59)
+            else if (hour == 15 && minute >= 30)
                 return 18;
-            else if (hour == 17 && minute >= 0 && minute <= 59)
+            else if (hour == 16)
+                return 18;
+            else if (hour == 17)
                 return 13;
             else if (hour == 18 && minute >= 0 && minute <= 29)
                 return 8;
