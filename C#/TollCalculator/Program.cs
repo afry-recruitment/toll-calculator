1090c3e7fbe78132a0b871da1d05085f8ebba1df
anas.b.hamda <anas.b.hamdan@outlook.com>
2022-09-25 21:30:06 +0200

    Minor code refactor

--- C#/TollCalculator/Program.cs
+++ C#/TollCalculator/Program.cs
@@ -4,9 +4,7 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        var date = new DateTime(2022, 09, 23, 06, 25, 20);
-        var date1 = new DateTime(2022, 09, 23, 06, 35, 20);
-        var dates = new List<DateTime> { DateTime.Now, date, date1 };
+        var dates = new List<DateTime> { DateTime.Now, new DateTime(2022, 09, 23, 06, 25, 20), new DateTime(2022, 09, 23, 06, 35, 20) };
         var vehicle = new Car();
 
         var result = new FeeCalculator().GetTollFee(vehicle, dates);
--- C#/TollCalculatorTesting/Seeds/FeeCalculatorSeeds.cs
+++ C#/TollCalculatorTesting/Seeds/FeeCalculatorSeeds.cs
@@ -23,8 +23,7 @@ public class FeeCalculatorSeeds
     {
         return new List<DateTime>(){
             new DateTime(2022, 09, 26, 7, 15, 0),
-            new DateTime(2022, 09, 26, 15, 45, 0)
-        };
+            new DateTime(2022, 09, 26, 15, 45, 0)};
     }
 
     public List<DateTime> GetDublicatHoursPeriods()
@@ -34,8 +33,7 @@ public class FeeCalculatorSeeds
             new DateTime(2022, 09, 26, 6, 35, 0),
             new DateTime(2022, 09, 26, 9, 15, 0),
             new DateTime(2022, 09, 26, 9, 35, 0),
-            new DateTime(2022, 09, 26, 18, 15, 0)
-        };
+            new DateTime(2022, 09, 26, 18, 15, 0)};
     }
 
     public List<DateTime> GetManyPeriods()
@@ -50,8 +48,7 @@ public class FeeCalculatorSeeds
             new DateTime(2022, 09, 26, 6, 35, 0),
             new DateTime(2022, 09, 26, 10, 15, 0),
             new DateTime(2022, 09, 26, 12, 35, 0),
-            new DateTime(2022, 09, 26, 18,15, 0)
-        };
+            new DateTime(2022, 09, 26, 18,15, 0)};
     }
 
 }
