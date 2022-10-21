c9a82465c71f7ded8f1e9171fd325f9a79a93b73
ItsAnass <domanee@hotmail.com>
2022-10-21 10:42:32 +0200

    Done

--- C#/TollCalculator.cs
+++ C#/TollCalculator.cs
@@ -1,9 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Globalization;
+using System.IO;
 using TollFeeCalculator;
 
 public class TollCalculator
 {
+
+    public string[] TollFreeVehicles = { "Motorbike", "Tractor", "Emergency", "Diplomat", "Foreign", "Military" };
+    private string path = @"C:\RedDayDates.txt";
 
     /**
      * Calculate the total toll fee for one day
@@ -13,96 +18,101 @@ public class TollCalculator
      * @return - the total toll fee for that day
      */
 
-    public int GetTollFee(Vehicle vehicle, DateTime[] dates)
+    public int GetTollFees(string vehicle, DateTime[] dates)
     {
-        DateTime intervalStart = dates[0];
+        DateTime intStart = dates[0];
+        int maxFee = 0;
+
         int totalFee = 0;
-        foreach (DateTime date in dates)
+        for (int i = 0; i < dates.Length; i++)
         {
-            int nextFee = GetTollFee(date, vehicle);
-            int tempFee = GetTollFee(intervalStart, vehicle);
+            int tempFee = 0;
 
-            long diffInMillies = date.Millisecond - intervalStart.Millisecond;
-            long minutes = diffInMillies/1000/60;
+            var timeDifference = dates[i].Subtract(intStart);
 
-            if (minutes <= 60)
+            if (timeDifference.TotalMinutes > 60)
             {
-                if (totalFee > 0) totalFee -= tempFee;
-                if (nextFee >= tempFee) tempFee = nextFee;
-                totalFee += tempFee;
+                totalFee += maxFee;
+                intStart = dates[i];
+                maxFee = GetTollFee(dates[i], vehicle);
             }
             else
             {
-                totalFee += nextFee;
+                tempFee = GetTollFee(dates[i], vehicle);
+                if (tempFee > maxFee) maxFee = tempFee;
             }
         }
+
+        totalFee += maxFee;
+
         if (totalFee > 60) totalFee = 60;
         return totalFee;
     }
 
-    private bool IsTollFreeVehicle(Vehicle vehicle)
+    private bool IsTollFreeVehicle(string vehicle)
     {
-        if (vehicle == null) return false;
-        String vehicleType = vehicle.GetVehicleType();
-        return vehicleType.Equals(TollFreeVehicles.Motorbike.ToString()) ||
-               vehicleType.Equals(TollFreeVehicles.Tractor.ToString()) ||
-               vehicleType.Equals(TollFreeVehicles.Emergency.ToString()) ||
-               vehicleType.Equals(TollFreeVehicles.Diplomat.ToString()) ||
-               vehicleType.Equals(TollFreeVehicles.Foreign.ToString()) ||
-               vehicleType.Equals(TollFreeVehicles.Military.ToString());
+        for (int i = 0; i < TollFreeVehicles.Length; i++)
+        {
+            if (TollFreeVehicles[i].ToLower().Contains(vehicle.ToLower())) return true;
+        }
+        return false;
     }
 
-    public int GetTollFee(DateTime date, Vehicle vehicle)
+
+    private int GetTollFee(DateTime date, string vehicle)
     {
         if (IsTollFreeDate(date) || IsTollFreeVehicle(vehicle)) return 0;
 
         int hour = date.Hour;
         int minute = date.Minute;
 
-        if (hour == 6 && minute >= 0 && minute <= 29) return 8;
-        else if (hour == 6 && minute >= 30 && minute <= 59) return 13;
-        else if (hour == 7 && minute >= 0 && minute <= 59) return 18;
-        else if (hour == 8 && minute >= 0 && minute <= 29) return 13;
-        else if (hour >= 8 && hour <= 14 && minute >= 30 && minute <= 59) return 8;
-        else if (hour == 15 && minute >= 0 && minute <= 29) return 13;
-        else if (hour == 15 && minute >= 0 || hour == 16 && minute <= 59) return 18;
-        else if (hour == 17 && minute >= 0 && minute <= 59) return 13;
-        else if (hour == 18 && minute >= 0 && minute <= 29) return 8;
+        if (hour == 6 && minute <= 29) return 8;
+        else if (hour == 6 && minute <= 59) return 13;
+        else if (hour == 7 && minute <= 59) return 18;
+        else if (hour == 8 && minute <= 29) return 13;
+        else if (hour >= 8 && hour <= 14 && minute <= 59) return 8;
+        else if (hour == 15 && minute <= 29) return 13;
+        else if ((hour >= 15 && hour <= 16 && minute <= 59)) return 18;
+        else if (hour == 17 && minute <= 59) return 13;
+        else if (hour == 18 && minute <= 29) return 8;
         else return 0;
+
     }
 
     private Boolean IsTollFreeDate(DateTime date)
     {
-        int year = date.Year;
-        int month = date.Month;
-        int day = date.Day;
+        if (date.DayOfWeek == DayOfWeek.Saturday || date.DayOfWeek == DayOfWeek.Sunday || IsRedDay(date)) return true;
 
-        if (date.DayOfWeek == DayOfWeek.Saturday || date.DayOfWeek == DayOfWeek.Sunday) return true;
-
-        if (year == 2013)
-        {
-            if (month == 1 && day == 1 ||
-                month == 3 && (day == 28 || day == 29) ||
-                month == 4 && (day == 1 || day == 30) ||
-                month == 5 && (day == 1 || day == 8 || day == 9) ||
-                month == 6 && (day == 5 || day == 6 || day == 21) ||
-                month == 7 ||
-                month == 11 && day == 1 ||
-                month == 12 && (day == 24 || day == 25 || day == 26 || day == 31))
-            {
-                return true;
-            }
-        }
         return false;
     }
 
-    private enum TollFreeVehicles
+
+
+    private Boolean IsRedDay(DateTime date)
     {
-        Motorbike = 0,
-        Tractor = 1,
-        Emergency = 2,
-        Diplomat = 3,
-        Foreign = 4,
-        Military = 5
+
+        List<DateTime> Holiday = new List<DateTime>();
+
+        if (date.DayOfWeek == DayOfWeek.Saturday || date.DayOfWeek == DayOfWeek.Sunday) return true;
+
+        using (StreamReader sr = new StreamReader(path))
+        {
+            while (sr.Peek() >= 0)
+            {
+                string str;
+                string[] strArray;
+                str = sr.ReadLine();
+
+                strArray = str.Split(',');
+
+                Holiday.Add(new DateTime(date.Year, int.Parse(strArray[0]), int.Parse(strArray[1])));
+
+            }
+        }
+        if (Holiday.Contains(date.Date)) return true;
+
+        
+        return false;
+
     }
 }
