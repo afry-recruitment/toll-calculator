862916e94117c377b9c828a14a7bf221ba82de12
Adam-Abovarda <117359186+Adam-Abovarda@users.noreply.github.com>
2022-11-06 15:30:07 +0100

    Update Vehicle.java

--- Java/Vehicle.java
+++ Java/Vehicle.java
@@ -1,5 +1,9 @@
 
 public interface Vehicle {
+		
 
-  public String getType();
+  public String getType(); //To provide the type of the vehicle
+  public void setType(String type); // To change the type of the vehicle
+  
 }
+
