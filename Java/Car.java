61702835e0a06ec3df8d8fe3ebde0de0bea63788
Adam-Abovarda <117359186+Adam-Abovarda@users.noreply.github.com>
2022-11-06 15:26:23 +0100

    Update Car.java

--- Java/Car.java
+++ Java/Car.java
@@ -1,7 +1,13 @@
 
 public class Car implements Vehicle {
-  @Override
-  public String getType() {
-    return "Car";
+public String type = "";
+@Override
+  
+  public String getType() { // To provide the type of the car
+    return type;
+  }
+  public void setType(String type) { //to change the type of the car
+	 this.type=type; 
+	  
   }
 }
