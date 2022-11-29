30ec5a8439ff211d51894bc771cdeae58db80b08
Raviteja Mulukuntla <ravitejamulukuntla@gmail.com>
2022-11-29 11:04:01 +0530

    Initial Commit

---
+++ C#/toll-calculator/toll-calculator/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TollFeeCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Vehicle car = new Car();
+            Vehicle bike = new Motorbike();
+
+            TollCalculator tollCalculator = new TollCalculator();
+
+            DateTime dateTime1 = new DateTime(2022, 1, 17, 10, 20, 55);
+            DateTime dateTime2 = new DateTime(2022, 1, 17, 14, 40, 05);
+            DateTime dateTime3 = new DateTime(2022, 1, 17, 15, 20, 20);
+            DateTime dateTime4 = new DateTime(2022, 1, 17, 17, 20, 30);
+            DateTime dateTime5 = new DateTime(2022, 1, 19, 10, 20, 30);
+            DateTime dateTime6 = new DateTime(2022, 1, 20, 05, 20, 30);
+            DateTime dateTime7 = new DateTime(2022, 1, 17, 20, 20, 55);
+
+            DateTime[] dates = { dateTime1, dateTime2, dateTime3, dateTime4, dateTime5, dateTime6, dateTime7 };
+
+            // Car
+            Console.WriteLine(tollCalculator.GetTollFee(car, dates));
+            // Bike
+            Console.WriteLine(tollCalculator.GetTollFee(bike, dates));
+        }
+    }
+}
---
+++ C#/toll-calculator/toll-calculator/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("toll-calculator")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("toll-calculator")]
+[assembly: AssemblyCopyright("Copyright © HP 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("be2b4b72-8247-4b35-a517-f39e2b21fdb7")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
