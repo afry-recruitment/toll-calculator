941e9bbba11b1c29f2cdf4d3c0b13162853d6d67
praveenwanninayake <123031648+praveenwanninayake@users.noreply.github.com>
2023-01-19 13:32:23 +0100

    Add files via upload

---
+++ C#/LogBase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TollFeeCalculator
+{
+    public abstract class LogBase
+    {
+        public abstract void Log(string Message);
+       
+    }
+}
---
+++ C#/Logger.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TollFeeCalculator
+{
+    public class Logger : LogBase
+    {
+        private string CurrentDirectory
+        {
+            get;
+            set;
+        }
+
+        private string FileName
+        {
+            get;
+            set;
+        }
+
+        private string FilePath
+        {
+            get;
+            set;
+        }
+
+        public Logger()
+        {
+            this.CurrentDirectory = Directory.GetCurrentDirectory();
+            this.FileName = "Log.txt";
+            this.FilePath = this.CurrentDirectory + "/" + this.FileName;
+        }
+
+        public override void Log(string Message)
+        {
+            using (StreamWriter w = File.AppendText(this.FilePath))
+            {
+                w.Write("\r\nLog Entry : ");
+                w.WriteLine("{0} {1}", DateTime.Now.ToLongTimeString(), DateTime.Now.ToLongDateString());
+                w.WriteLine("{0}" , Message);
+                w.WriteLine("---------------------------------");
+
+            }
+        }
+    }
+}
