dd358ff2a0cb38a315c54375e5f977134a53c7d6
Carpenteri1 <niclastimle@icloud.com>
2023-03-29 22:05:51 +0200

    data for consoleClient

--- C#/toll-calculator/ConsoleClient/Program.cs
+++ C#/toll-calculator/ConsoleClient/Program.cs
@@ -1,2 +1,48 @@
 ﻿using ConsoleClient;
-Console.WriteLine("hey");
+using ConsoleClient.Repo;
+
+TollCalculator tollCalculator = new TollCalculator();
+
+for (int i = 1; i <= 11; i++)
+{
+    foreach (var vehicle in InMemoryRepo.LoadVehicles())
+    {
+        switch (i)
+        {
+            case 1:
+                Console.WriteLine($"Weekend: {vehicle.GetVehicleType()}: {vehicle.RegNumber} payed fee of {tollCalculator.GetTollFee(vehicle, InMemoryRepo.LoadNewDateAndTimes(i))} kr");
+                break;
+            case 2:
+                Console.WriteLine($"WeekDay: {vehicle.GetVehicleType()}: {vehicle.RegNumber} payed fee of {tollCalculator.GetTollFee(vehicle, InMemoryRepo.LoadNewDateAndTimes(i))} kr");
+                break;
+            case 3:
+                Console.WriteLine($"LongFriday: {vehicle.GetVehicleType()}: {vehicle.RegNumber} payed fee of {tollCalculator.GetTollFee(vehicle, InMemoryRepo.LoadNewDateAndTimes(i))} kr");
+                break;
+            case 4:
+                Console.WriteLine($"Weekday {vehicle.GetVehicleType()}: {vehicle.RegNumber} payed fee of {tollCalculator.GetTollFee(vehicle, InMemoryRepo.LoadNewDateAndTimes(i))} kr");
+                break;
+            case 5:
+                Console.WriteLine($"FirstDayOfMay: {vehicle.GetVehicleType()}: {vehicle.RegNumber} payed fee of {tollCalculator.GetTollFee(vehicle, InMemoryRepo.LoadNewDateAndTimes(i))} kr");
+                break;
+            case 6:
+                Console.WriteLine($"AscensionDay: {vehicle.GetVehicleType()}: {vehicle.RegNumber} payed fee of {tollCalculator.GetTollFee(vehicle, InMemoryRepo.LoadNewDateAndTimes(i))} kr");
+                break;
+            case 7:
+                Console.WriteLine($"EasterMonday: {vehicle.GetVehicleType()}: {vehicle.RegNumber} payed fee of {tollCalculator.GetTollFee(vehicle, InMemoryRepo.LoadNewDateAndTimes(i))} kr");
+                break;
+            case 8:
+                Console.WriteLine($"NatonalDay: {vehicle.GetVehicleType()}: {vehicle.RegNumber} payed fee of {tollCalculator.GetTollFee(vehicle, InMemoryRepo.LoadNewDateAndTimes(i))} kr");
+                break;
+            case 9:
+                Console.WriteLine($"Weekdays: {vehicle.GetVehicleType()}: {vehicle.RegNumber} payed fee of {tollCalculator.GetTollFee(vehicle, InMemoryRepo.LoadNewDateAndTimes(i))} kr");
+                break;
+            case 10:
+                Console.WriteLine($"ChristmessDays: {vehicle.GetVehicleType()}: {vehicle.RegNumber} payed fee of {tollCalculator.GetTollFee(vehicle, InMemoryRepo.LoadNewDateAndTimes(i))} kr");
+                break;
+            case 11:
+                Console.WriteLine($"Weekday: {vehicle.GetVehicleType()}: {vehicle.RegNumber} payed fee of {tollCalculator.GetTollFee(vehicle, InMemoryRepo.LoadNewDateAndTimes(i))} kr");
+                break;
+        }
+    }
+    Console.WriteLine();
+}
