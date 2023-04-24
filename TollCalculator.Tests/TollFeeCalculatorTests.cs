f552d55c27b7808563379be43770950287f28c34
NiklasNegri <niklas.negri@gmail.com>
2023-04-24 11:00:23 +0200

    Fix test and solution error

--- TollCalculator.Tests/TollFeeCalculatorTests.cs
+++ TollCalculator.Tests/TollFeeCalculatorTests.cs
@@ -7,10 +7,10 @@ namespace TollCalculator.Tests
         [Fact]
         public void NewTollFee_WithTollFreeVehicle_ShouldReturnTollFeeAmountZero()
         {
-            var car = new Car("ABC-123", Vehicle.VehicleType.Military);
+            var carTollFree = new Car("ABC-123", Vehicle.VehicleSector.Military);
             var tollDate = new DateTime(2023, 1, 2, 6, 15, 0);
 
-            var result = TollFeeCalculator.NewTollFee(car, tollDate);
+            var result = TollFeeCalculator.NewTollFee(carTollFree, tollDate);
 
             Assert.Equal(0, result.TollFeeAmount);
         }
@@ -18,10 +18,10 @@ namespace TollCalculator.Tests
         [Fact]
         public void NewTollFee_WithTollFreeDate_ShouldReturnTollFeeAmountZero()
         {
-            var car = new Car("ABC-123", Vehicle.VehicleType.Car);
-            var tollDate = new DateTime(2023, 1, 1, 6, 15, 0);
+            var car = new Car("ABC-123", Vehicle.VehicleSector.Civilian);
+            var tollFreeDate = new DateTime(2023, 1, 1, 6, 15, 0);
 
-            var result = TollFeeCalculator.NewTollFee(car, tollDate);
+            var result = TollFeeCalculator.NewTollFee(car, tollFreeDate);
 
             Assert.Equal(0, result.TollFeeAmount);
         }
@@ -29,7 +29,7 @@ namespace TollCalculator.Tests
         [Fact]
         public void NewTollFee_WithoutTollFreeVehicle_WithoutTollFreeDate_ShouldNotReturnTollFeeAmountZero()
         {
-            var car = new Car("ABC-123", Vehicle.VehicleType.Car);
+            var car = new Car("ABC-123", Vehicle.VehicleSector.Civilian);
             var tollDate = new DateTime(2023, 1, 2, 6, 15, 0);
 
             var result = TollFeeCalculator.NewTollFee(car, tollDate);
