cb565a4abfca48e5efb2458013e9230205840f52
NiklasNegri <niklas.negri@gmail.com>
2023-04-24 10:14:45 +0200

    Refactor custom exceptions

---
+++ TollCalculator/Exceptions/TollDateDayException.cs
@@ -0,0 +1,14 @@
+﻿namespace TollCalculator.Exceptions
+{
+    [Serializable]
+    public class TollDateDayException : ApplicationException
+    {
+        public TollDateDayException() { }
+
+        public TollDateDayException(string message)
+            : base(message) { }
+
+        public TollDateDayException(string message, Exception innerException)
+            : base(message, innerException) { }
+    }
+}
