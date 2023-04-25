0fcec8abc8daf00399ea16f32bd3d594d413be37
Padmasankha Gurusinghe <padmasankha@192.168.1.3>
2023-04-25 19:40:14 +0530

    Initial springboot project setup

---
+++ src/test/java/com/afry/tollcalculator/TollCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.afry.tollcalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TollCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
