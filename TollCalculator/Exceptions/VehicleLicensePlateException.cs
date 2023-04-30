909d915e7b6a427191d9893366a69f06282cb5cb
NiklasNegri <niklas.negri@gmail.com>
2023-04-30 17:35:42 +0200

    Add custom exception

---
+++ TollCalculator/Exceptions/VehicleLicensePlateException.cs
@@ -0,0 +1,14 @@
+﻿namespace TollCalculator.Exceptions
+{
+    [Serializable]
+    public class VehicleLicensePlateException : ApplicationException
+    {
+        public VehicleLicensePlateException() { }
+
+        public VehicleLicensePlateException(string message)
+            : base(message) { }
+
+        public VehicleLicensePlateException(string message, Exception innerException)
+            : base(message, innerException) { }
+    }
+}
