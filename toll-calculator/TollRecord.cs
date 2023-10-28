8245f8f9e3b7dc080247860a92b5a67ddcfa09f7
ForlornU <101473036+ForlornU@users.noreply.github.com>
2023-10-28 06:52:57 +0200

    Added Solution
    New Visual Studio solution

---
+++ toll-calculator/TollRecord.cs
@@ -0,0 +1,12 @@
+﻿
+public class TollRecord
+{
+    public Vehicle vehicle;
+    public DateTime[] instances;
+
+    public TollRecord(Vehicle v, DateTime[] t) 
+    {
+        vehicle = v;
+        instances = t;
+    }
+}
---
+++ toll-calculator/Vehicle.cs
@@ -0,0 +1,13 @@
+﻿public enum VehicleType { Car, Truck, Bike, Trike, UFO, Motorbike, Tractor, Emergency, Diplomat, Foreign, Military, Unidentified }
+
+public class Vehicle
+{
+    public string licensePlate = "Undefined";
+    public VehicleType vehicleType = VehicleType.Car;
+
+    public Vehicle(string plate, VehicleType type) 
+    {
+        licensePlate = plate;
+        vehicleType = type;
+    }
+}
